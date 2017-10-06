2a7b0065d7887962c39f1186be1002f0bbb2806b
Guillaume HARARI <guillaumeharari@hotmail.com>
2017-10-06 13:51:51 +0200

    Directory and first files created

---
+++ inc/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">
+<head>
+    <title>Find The Precious</title>
+</head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/style.css" />
+<body>
+
+</body>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php require 'inc/head.php'; ?>
+<h1>AHHHH</h1>
+
+
+
+
+
+
+<?php require 'inc/foot.php'; ?>
